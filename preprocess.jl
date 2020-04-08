868a350e5e753356a47e281d322ceed89040c97e
kiristern <kiri.a.stern@gmail.com>
2020-04-08 12:00:21 -0400

    create training and testing sets

--- preprocess.jl
+++ preprocess.jl
@@ -28,6 +28,7 @@ nan_to_0(Resistance)
 nan_to_0(Origin)
 nan_to_0(Connectivity)
 
+#create Training dataset
 # Extract and vectorize 150 random 10x10 resistance, origin, and connectivity layers
 Random.seed!(1234)
 Stride = 10
@@ -43,5 +44,24 @@ for i in rand(10:950, 150), j in rand(10:950, 150)
   if minimum(y) > 0 #predict only when there is connectivity
     push!(imgs, x)
     push!(labels, y)
+  end
+end
+
+#create Testing dataset
+# Extract and vectorize 150 random 10x10 resistance, origin, and connectivity layers
+Random.seed!(5678)
+Stride = 10
+test_imgs = []
+test_labels = []
+
+for i in rand(10:950, 150), j in rand(10:950, 150)
+  #taking groups of matrices of dimensions StridexStride
+  x_res = Resistance[i:(i+Stride-1),j:(j+Stride-1)]
+  x_or = Origin[i:(i+Stride-1),j:(j+Stride-1)]
+  x = cat(x_res, x_or, dims=3) #concatenate resistance and origin vectors
+  y = Connectivity[i:(i+Stride-1),j:(j+Stride-1)] #matrix we want to predict
+  if minimum(y) > 0 #predict only when there is connectivity
+    push!(test_imgs, x)
+    push!(test_labels, y)
   end
 end
