bd0f09bac5dc6309ab4bc3899c8b224b941b300d
kiristern <kiri.a.stern@gmail.com>
2020-04-15 21:25:00 -0400

    L2 loss function

--- train_model.jl
+++ train_model.jl
@@ -13,8 +13,8 @@ if has_cuda()
 end
 
 #if non-minibatching
-training_set = collect(zip(train_maps, train_connect))
-validating_set = collect(zip(valid_maps, valid_connect))
+# training_set = collect(zip(train_maps, train_connect))
+# validating_set = collect(zip(valid_maps, valid_connect))
 
 train_set
 validation_set
@@ -36,20 +36,23 @@ model = Chain(
     x -> reshape(x, (Stride, Stride, 1, batch_size))
 )
 
-model2 = Chain(
-    #Apply a Conv layer to a 2-channel input using a 2x2 window size, giving a 16-channel output. Output is activated by relu
-    Conv((3,3), 2=>16, pad=(1,1), relu),
-    MaxPool((2,2)),
-    #2x2 window slides over x reducing it to half the size while retaining most important feature information for learning (takes highest/max value)
-    Conv((3,3), 16=>32, pad=(1,1), relu),
-    MaxPool((2,2)),
+#  model2 = Chain(
+#     #Apply a Conv layer to a 2-channel input using a 2x2 window size, giving a 16-channel output. Output is activated by relu
+#     Conv((3,3), 2=>16, pad=(1,1), relu),
+#     MaxPool((2,2)),
+#     #2x2 window slides over x reducing it to half the size while retaining most important feature information for learning (takes highest/max value)
+#     Conv((3,3), 16=>32, pad=(1,1), relu),
+#     MaxPool((2,2)),
+#
+#     Conv((3,3), 32=>9, pad=(1,1), relu),
+#     MaxPool((2,2)),
+#
+#     #flatten from 3D tensor to a 2D one, suitable for dense layer and training
+#     x -> reshape(x, (9, batch_size)),
+# )
 
-    Conv((3,3), 32=>9, pad=(1,1), relu),
-    MaxPool((2,2)),
 
-    #flatten from 3D tensor to a 2D one, suitable for dense layer and training
-    x -> reshape(x, (9, batch_size)),
-)
+
 
 #View layer outputs
 model[1](train_set[1][1]) #layer 1: 9x9x16x32
@@ -60,13 +63,13 @@ model[1:5](train_set[1][1]) #layer 5: 128x32
 model[1:6](train_set[1][1]) #layer 6: 81x32
 model[1:7](train_set[1][1]) #layer 7: 9x9x1x32
 
-model2[1](training_set[1][1]) #layer 1: 9x9x16x32
-model2[1:2](training_set[1][1]) #layer 2: 4x4x16x32
-model2[1:3](training_set[1][1]) #layer 3: 4x4x32x32
-model2[1:4](training_set[1][1]) #layer 4: 2x2x32x32
-model2[1:5](training_set[1][1]) #layer 5: 128x32
-model2[1:6](training_set[1][1]) #layer 6: 81x32
-model2[1:7](training_set[1][1]) #layer 7:
+# model2[1](training_set[1][1]) #layer 1: 9x9x16x32
+# model2[1:2](training_set[1][1]) #layer 2: 4x4x16x32
+# model2[1:3](training_set[1][1]) #layer 3: 4x4x32x32
+# model2[1:4](training_set[1][1]) #layer 4: 2x2x32x32
+# model2[1:5](training_set[1][1]) #layer 5: 128x32
+# model2[1:6](training_set[1][1]) #layer 6: 81x32
+# model2[1:7](training_set[1][1]) #layer 7:
 
 # Load model and datasets onto GPU, if enabled
 train_set = gpu.(train_set)
@@ -81,38 +84,13 @@ augment(x) = x .+ gpu(0.1f0*randn(eltype(x), size(x)))
 paramvec(m) = vcat(map(p->reshape(p, :), params(m))...)
 anynan(x) = any(isnan.(x))
 
-#=
-`loss()` calculates the crossentropy loss between our prediction `y_hat` (calculated from `model(x)`) and the ground truth `y`.  We augment the data a bit, adding gaussian random noise to our image to make it more robust.
-=#
-
-
-# loss NaN
-# ϵ = 1.0f-32
-# compare(y::Array, y′) = maximum(y′, dims = 4) .== maximum(y .* y′, dims = 4)
-# accuracy(x, y::Array) = mean(compare(y, m(x)))
-# function loss(x,y)
-#   ŷ = m(x)
-#   return crossentropy(ŷ .+ ϵ,y)
-# end
-#
-# function accuracy(data_set)
-#   batch_size = size(data_set[1][1])[end]
-#   l = length(data_set)*batch_size
-#   s = 0f0
-#   for (x,y::Array) in data_set
-#     s += sum(compare(y|>gpu, m(x|>gpu)))
-#   end
-#   return s/l
-# end
-
-training_set
-
 
 function loss(x, y)
     x̂ = augment(x)
     ŷ = model(x̂)
-    return crossentropy(ŷ, y)
+    return sum((y .- ŷ).^2)./prod(size(x̂)) #divided by the actual value
 end
+
 accuracy(x, y) = mean(onecold(cpu(model(x))) .== onecold(cpu(y)))
 
 # Train our model with the given training set using the ADAM optimizer and printing out performance against the validation set as we go.
@@ -186,3 +164,5 @@ p1 = heatmap(Z[1], title="predicted")
 p2 = heatmap(model(W[1]), title="observed")
 p3 = scatter(Z[1], model(W[1]), leg=false, c=:black, xlim=(0,1), ylim=(0,1), xaxis="observed", yaxis="predicted")
 plot(p1,p2, p3)
+
+cat()
