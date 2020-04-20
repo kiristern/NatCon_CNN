83b3252cded28e79f20eb333b0849276d9db745f
kiristern <kiri.a.stern@gmail.com>
2020-04-20 12:04:13 -0400

    fix model for 6x6

--- train_model.jl
+++ train_model.jl
@@ -29,9 +29,9 @@ model = Chain(
     MaxPool((2,2)),
 
     #flatten from 3D tensor to a 2D one, suitable for dense layer and training
-    x -> reshape(x, (128, batch_size)),
+    x -> reshape(x, (reshapeLayer, batch_size)),
 
-     Dense(128, 81),
+     Dense(reshapeLayer, Stride*Stride),
 
     #reshape to match output dimensions
     x -> reshape(x, (Stride, Stride, 1, batch_size))
@@ -42,6 +42,8 @@ model[1](train_set[1][1]) #layer 1: 9x9x16x32
 model[1:2](train_set[1][1]) #layer 2: 4x4x16x32
 model[1:3](train_set[1][1]) #layer 3: 4x4x32x32
 model[1:4](train_set[1][1]) #layer 4: 2x2x32x32
+ls = model[1:4](train_set[1][1])
+reshapeLayer = size(ls,1)*size(ls,2)*size(ls,3)
 model[1:5](train_set[1][1]) #layer 5: 128x32
 model[1:6](train_set[1][1]) #layer 6: 81x32
 model[1:7](train_set[1][1]) #layer 7: 9x9x1x32
