0791b54fb24be0f90de119e1663fb540a5db10ea
kiristern <kiri.a.stern@gmail.com>
2020-04-22 19:27:12 -0400

    rename and changed some things

--- 9x9_preprocess.jl
+++ 9x9_preprocess.jl
@@ -2,8 +2,12 @@
 
 Get a 27x27 sample from original data maps and, using the trained (working) model, predict nine 9x9 images and "stitch" together in 3x3 batch to get a final 27x27 image.
 
+n 27x27 images; aka, groups of 3 (9x9) by 3 (9x9) images
 Output:
-    one 27x27 image; aka, 3 (9x9) by 3 (9x9) images
+  train_9x9_maps: 9x9x2
+  train_9x9_connect: 9x9x2
+  validation_maps : 27x27x2
+  validation_connect: 27x72
 
 =#
 cd(@__DIR__)
@@ -51,7 +55,6 @@ for i in coordinates
   end
 end
 
-[first(range[1][1]):Stride:last(range[1][1])+1]
 
 #get every single index in samples
 x_indices = []
@@ -67,67 +70,21 @@ end
 x_idxes = x_indices[1:Stride:end]
 y_idxes = y_indices[1:Stride:end]
 
-
-
-
-
-
-
-
-
-
-#make a function get indices of each sample point
-indices = []
-CartesianIndices(res[])
-findall(x->x==2, res[1])
-
-for i in res, j in res
-  idx = CartesianIndices(res[i][j])
-  push!(indices, idx)
-end
-
-#from 27x27 samples, generate nine, 9x9 subsamples
-subsamp_in = []
-subsamp_out = []
-for ss in test_input, s in test_input
-  xr = Resistance[ss:(ss+Stride-1), s:(s+Stride-1)]
-  xo = Origin[ss:(ss+Stride-1), s:(s+Stride-1)]
-  X = cat(xr, xo, dims=3)
-  Y = Connectivity[ss:(ss+Stride-1), s:(s+Stride-1)]
-  push!(subsamp_in, X)
-  push!(subsamp_out, Y)
-end
-
-
-#get all cartesian points for range
-Px_range = []
-for i in 1:length(range)
-  u = collect(range[i][1])
-  push!(Px_range, u)
-end
-Px_range = vcat(Px_range...)
-
-Py_range = []
-for i in 1:length(range)
-  u = collect(range[i][2])
-  push!(Py_range, u)
-end
-Py_range = vcat(Py_range...)
-
-
-#moving window
-#training data within range
-X = []
-Y = []
-for i in Px_range, j in Py_range
-  xr = vec(r[i:(i+stride-1),j:(j+stride-1)])
-  xo = vec(o[i:(i+stride-1),j:(j+stride-1)])
-  x = vcat(xr, xo) #stack the matrices together
-  y = c[i:(i+stride-1),j:(j+stride-1)] #matrix we want to predict
+#create 9x9 samples
+train_9x9_maps = []
+train_9x9_connect = []
+for i in x_idxes, j in y_idxes
+  x_res = Resistance[i:(i+Stride-1),j:(j+Stride-1)]
+  x_or = Origin[i:(i+Stride-1),j:(j+Stride-1)]
+  x = cat(x_res, x_or, dims=3) #concatenate resistance and origin layers
+  y = Connectivity[i:(i+desired-1),j:(j+desired-1)] #matrix we want to predict
   if minimum(y) > 0 #predict only when there is connectivity
-    push!(X, x)
-    push!(Y, y)
+    push!(train_9x9_maps, x)
+    push!(train_9x9_connect, y)
   end
 end
-X
-Y
+
+train_9x9_maps
+train_9x9_connect
+validation_maps
+validation_connect
