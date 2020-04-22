c62af69efa8a9d012df6ced9e7ef2705fcc9d86a
kiristern <kiri.a.stern@gmail.com>
2020-04-22 16:30:10 -0400

    validation 27x27 maps and connect

--- train_model.jl
+++ train_model.jl
@@ -126,7 +126,7 @@ run = @time @elapsed for epoch_idx in 1:5
     # If this is the best accuracy we've seen so far, save the model out
     if acc >= best_acc
         @info(" -> New best accuracy! Saving model out to connectivity.bson")
-        BSON.@save joinpath(dirname(@__FILE__), "BSON/$(Stride)x$(Stride).bson") params=cpu.(params(model)) epoch_idx acc
+        BSON.@save joinpath(dirname(@__FILE__), "BSON/overwrite_$(Stride)x$(Stride).bson") params=cpu.(params(model)) epoch_idx acc
         best_acc = acc
         last_improvement = epoch_idx
     end
