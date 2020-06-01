b893e49a61411014e1640f4649872eaaf95ccc75
kiristern <kiri.a.stern@gmail.com>
2020-06-01 13:01:42 -0400

    run model on all samples (not just first)

--- test_model_on_new_data.jl
+++ test_model_on_new_data.jl
@@ -23,9 +23,9 @@ begin
     print("## Plotting...  ##")
     print("##################")
 end
-p1 = heatmap(validation_set[1][2][:,:,1,2], title="predicted")
-p2 = heatmap(model(validation_set[1][1])[:,:,1,2], title="observed")
-p3 = scatter(validation_set[1][2][:,:,1,2], model(validation_set[1][1])[:,:,1,2], leg=false, c=:black, xlim=(0,1), ylim=(0,1), xaxis="observed", yaxis="predicted")
+p1 = heatmap(validation_set[1][2][:,:,1,1], title="predicted") #connectivity map
+p2 = heatmap(model(validation_set[1][1])[:,:,1,1], title="observed") #resistance and origin layer map
+p3 = scatter(validation_set[1][2][:,:,1,1], model(validation_set[1][1])[:,:,1,2], leg=false, c=:black, xlim=(0,1), ylim=(0,1), xaxis="observed", yaxis="predicted")
 plot(p1,p2, p3)
 savefig("figures/$(Stride)x$(Stride)_$(run)sec_$(best_acc*100)%.png")
 
@@ -42,39 +42,48 @@ validation_connect: vector of m elements of dims 27x27
 
 =#
 
+model_on_9x9 = model(nine_nine[1][1])
 
+m1 = model_on_9x9[:,:,1,1]
+m2 = model_on_9x9[:,:,1,2]
+m3 = model_on_9x9[:,:,1,3]
+row1 = hcat(m1,m2,m3)
 
-maps9x9
-connect9x9
-validation_maps
-validation_connect
+m4 = model_on_9x9[:,:,1,4]
+m5 = model_on_9x9[:,:,1,5]
+m6 = model_on_9x9[:,:,1,6]
+row2 = hcat(m4,m5,m6)
 
+m7 = model_on_9x9[:,:,1,7]
+m8 = model_on_9x9[:,:,1,8]
+m9 = model_on_9x9[:,:,1,9]
+row3 = hcat(m7,m8,m9)
 
+map27x27 = vcat(row1,row2,row3)
+testmap1 = heatmap(map27x27)
 
-#Stitching together 9 9x9 maps and compare to 27x27 map
-v2 = validation_set2
+#connectivity map (true map -- not run with model)
+truem1 = nine_nine[1][2][:,:,1,1]
+truem2 = nine_nine[1][2][:,:,1,2]
+truem3 = nine_nine[1][2][:,:,1,3]
+truerow1 = hcat(truem1,truem2,truem3)
 
-model(v2[1][1])[:,:,1,2]
-model(v2[1][1])
+truem4 = nine_nine[1][2][:,:,1,4]
+truem5 = nine_nine[1][2][:,:,1,5]
+truem6 = nine_nine[1][2][:,:,1,6]
+truerow2 = hcat(truem4,truem5,truem6)
 
-heatmap(v2[1][2][:,:,1,1])
-heatmap(v2[1][2][:,:,1,2])
-heatmap(v2[1][2][:,:,1,3])
+truem7 = nine_nine[1][2][:,:,1,7]
+truem8 = nine_nine[1][2][:,:,1,8]
+truem9 = nine_nine[1][2][:,:,1,9]
+truerow3 = hcat(truem7,truem8,truem9)
 
+truemap27x27 = vcat(truerow1, truerow2, truerow3)
+truemap1 = heatmap(truemap27x27)
 
+comparemap1 = heatmap(compare_with_27x27[1][2][:,:,1,1]) #want to compare to the true connectivity map at the same index points
 
-v1 = validation_connect
-heatmap(v1[1])
+refmaps = plot(truemap1, comparemap1)
 
-
-#have a look
-begin
-    print("##################")
-    print("## Plotting...  ##")
-    print("##################")
-end
-p1 = heatmap(validation_set2[1][2][:,:,1,2], title="predicted")
-p2 = heatmap(model(validation_set2[1][1])[:,:,1,2], title="observed")
-p3 = scatter(validation_set2[1][2][:,:,1,2], model(validation_set2[1][1])[:,:,1,2], leg=false, c=:black, xlim=(0,1), ylim=(0,1), xaxis="observed", yaxis="predicted")
-plot(p1,p2, p3)
-savefig("figures/r2_$(Stride)x$(Stride)_$(run)sec_$(best_acc*100)%.png")
+scatterplotmaps = scatter(map27x27, truemap27x27, leg=false, c=:black, xlim=(0,1), ylim=(0,1), xaxis="observed", yaxis="predicted")
+plot(testmap1, truemap1, comparemap1, scatterplotmaps)
