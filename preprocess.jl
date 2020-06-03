beaaf2740e1070ecae54178357739dd95511f95f
kiristern <kiri.a.stern@gmail.com>
2020-06-03 15:58:40 -0400

    fix spacing

--- preprocess.jl
+++ preprocess.jl
@@ -37,9 +37,9 @@ nan_to_0(Connectivity)
 # Extract 150 random 9x9 resistance, origin, and connectivity layers
 Random.seed!(1234)
 Stride = 9
+
 maps = []
 connect = []
-
 for i in rand(10:950, 150), j in rand(10:950, 150)
   #taking groups of matrices of dimensions StridexStride
   x_res = Resistance[i:(i+Stride-1),j:(j+Stride-1)]
@@ -54,9 +54,9 @@ end
 
 #create Testing dataset
 Random.seed!(5678)
+
 test_maps = []
 test_connect = []
-
 for i in rand(10:950, 150), j in rand(10:950, 150)
   #taking groups of matrices of dimensions StridexStride
   x_res = Resistance[i:(i+Stride-1),j:(j+Stride-1)]
