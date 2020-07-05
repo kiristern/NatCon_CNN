5c57455eac1418a674dde8d561cde318c6eb2eeb
kiristern <kiri.a.stern@gmail.com>
2020-07-05 13:06:14 -0400

    add comment

--- preprocess.jl
+++ preprocess.jl
@@ -36,7 +36,7 @@ Stride = 9
 
 maps = []
 connect = []
-for i in rand(10:950, 150), j in rand(10:950, 150) #TODO: try sampling from entire map ?
+for i in rand(10:950, 150), j in rand(10:950, 150) #TODO: try sampling from entire map ie. comment out line 45 (min>0)
   #taking groups of matrices of dimensions StridexStride
   x_res = Resistance[i:(i+Stride-1),j:(j+Stride-1)]
   x_or = Origin[i:(i+Stride-1),j:(j+Stride-1)]
