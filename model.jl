8918e9a8fd90b24dea7f90e3acb2c5c300326baa
kiristern <kiri.a.stern@gmail.com>
2020-07-08 11:03:51 -0400

    use flux.flatten to reshape 3d tensor to 2d instead of manually

--- model.jl
+++ model.jl
@@ -1,8 +1,8 @@
 # include("libraries.jl")
 # include("functions.jl")
-# include("preprocess.jl")
-# include("validation_dataset.jl")
-# include("minibatch.jl")
+include("preprocess.jl")
+include("validation_dataset.jl")
+include("minibatch.jl")
 
 # train_set
 # validation_set
@@ -23,7 +23,7 @@ m = Chain(
 inputlayersize = Array{Float32}(undef, 9, 9, 2, 32)
 
 model = Chain(
-    #Apply a Conv layer to a 2-channel (R & O layer) input using a 2x2 window size, giving a 16-channel output. Output is activated by relu
+    #Apply a Conv layer to a 2-channel (R & O layer) input using a 3x3 window size, giving a 16-channel output. Output is activated by relu
     Conv((3,3), 2=>16, pad=(1,1), relu),
     MaxPool((2,2)),
     #2x2 window slides over x reducing it to half the size while retaining most important feature information for learning (takes highest/max value)
@@ -31,20 +31,19 @@ model = Chain(
     MaxPool((2,2)),
 
     #flatten from 3D tensor to a 2D one, suitable for dense layer and training
-    x -> reshape(x, (Int(prod(size(m[1:4](inputlayersize)))/batch_size), batch_size)),
-
-     Dense(Int(prod(size(m[1:4](inputlayersize)))/batch_size), Stride*Stride),
+    flatten,
+    Dense(Int(prod(size(m[1:4](inputlayersize)))/batch_size), Stride*Stride), #TODO: Try adding an activation function at the end of this layer?
 
     #reshape to match output dimensions
     x -> reshape(x, (Stride, Stride, 1, batch_size))
 )
 
 #View layer outputs
-# model[1](train_set[1][1]) #layer 1: 9x9x16x32
-# model[1:2](train_set[1][1]) #layer 2: 4x4x16x32
-# model[1:3](train_set[1][1]) #layer 3: 4x4x32x32
-# model[1:4](train_set[1][1]) #layer 4: 2x2x32x32
-# # reshape layer
-# model[1:5](train_set[1][1]) #layer 5: 128x32
-# model[1:6](train_set[1][1]) #layer 6: 81x32
-# model[1:7](train_set[1][1]) #layer 7: 9x9x1x32
+model[1](train_set[1][1]) #layer 1: 9x9x16x32
+model[1:2](train_set[1][1]) #layer 2: 4x4x16x32
+model[1:3](train_set[1][1]) #layer 3: 4x4x32x32
+model[1:4](train_set[1][1]) #layer 4: 2x2x32x32
+# reshape layer
+model[1:5](train_set[1][1]) #layer 5: 128x32
+model[1:6](train_set[1][1]) #layer 6: 81x32
+model[1:7](train_set[1][1]) #layer 7: 9x9x1x32
