1d3ab64c2f696a2eda9496dbb9689f3408699de6
kiristern <kiri.a.stern@gmail.com>
2020-07-15 11:22:43 -0400

    modified model, relu sigma relu, 9 layers. seems to be the best so far..

--- model.jl
+++ model.jl
@@ -1,11 +1,12 @@
 # include("libraries.jl")
 # include("functions.jl")
-include("preprocess.jl")
-include("validation_dataset.jl")
-include("minibatch.jl")
+# include("preprocess.jl")
+# include("validation_dataset.jl")
+# include("minibatch.jl")
 
 # train_set
 # validation_set
+
 
 begin
     print("##########################")
@@ -16,8 +17,10 @@ end
 m = Chain(
     Conv((3,3), 2=>16, pad=(1,1), relu),
     MaxPool((2,2)),
-    Conv((3,3), 16=>32, pad=(1,1), relu),
-    MaxPool((2,2))
+    Conv((3,3), 16=>32, pad=(1,1), σ),
+    MaxPool((2,2)),
+    Conv((3,3), 32=>8, pad=(1,1), relu),
+    MaxPool((2,2)),
 )
 
 inputlayersize = Array{Float32}(undef, 9, 9, 2, 32)
@@ -27,18 +30,23 @@ model = Chain(
     Conv((3,3), 2=>16, pad=(1,1), relu),
     MaxPool((2,2)),
     #2x2 window slides over x reducing it to half the size while retaining most important feature information for learning (takes highest/max value)
-    Conv((3,3), 16=>32, pad=(1,1), relu),
+    Conv((3,3), 16=>32, pad=(1,1), σ),
+    MaxPool((2,2)),
+
+    Conv((3,3), 32=>8, pad=(1,1), relu),
     MaxPool((2,2)),
 
     #flatten from 3D tensor to a 2D one, suitable for dense layer and training
     flatten,
-    Dense(Int(prod(size(m[1:4](inputlayersize)))/batch_size), Stride*Stride), #TODO: Try adding an activation function at the end of this layer?
+    Dense(Int(prod(size(m[1:6](inputlayersize)))/batch_size), Stride*Stride), #TODO: Try adding an activation function at the end of this layer?
 
     #reshape to match output dimensions
     x -> reshape(x, (Stride, Stride, 1, batch_size))
 )
 
-#View layer outputs
+
+
+# #View layer outputs
 model[1](train_set[1][1]) #layer 1: 9x9x16x32
 model[1:2](train_set[1][1]) #layer 2: 4x4x16x32
 model[1:3](train_set[1][1]) #layer 3: 4x4x32x32
@@ -47,3 +55,5 @@ model[1:4](train_set[1][1]) #layer 4: 2x2x32x32
 model[1:5](train_set[1][1]) #layer 5: 128x32
 model[1:6](train_set[1][1]) #layer 6: 81x32
 model[1:7](train_set[1][1]) #layer 7: 9x9x1x32
+model[1:8](train_set[1][1])
+model[1:9](train_set[1][1])
