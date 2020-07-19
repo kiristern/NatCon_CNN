e11a97f8fff1a12f1b14d64db78243de3b57d2dc
kiri <kiri.a.stern@gmail.com>
2020-07-19 11:51:18 -0400

    Update visualize150samples.jl
    remove unnecessary code

--- visualize150samples.jl
+++ visualize150samples.jl
@@ -23,48 +23,9 @@ for i in 1:length(samp_pts)
   push!(get_train_samp2, x)
 end
 
-# get_train_samp = rand(1:size(Origin,2)-Stride, 150)
-# get_train_samp2 = rand(Stride:size(Origin,2)-Stride, 150)
-
-
-
-# bbox=[]
-# for i in get_train_samp, j in get_train_samp2
-#   x_res = Resistance[i:(i+Stride-1),j:(j+Stride-1)]
-#   push!(bbox, x_res)
-# end
 
 #visualize samples retrieved for Training
 sample_pts = Tuple.(zip(get_train_samp, get_train_samp2))
-
-# samp_grids = []
-# for i in all_samp_pts
-#   y = Connectivity[first(i):first(i)+Stride-1,last(i):last(i)+Stride-1] #matrix we want to predict
-#     push!(samp_grids, y)
-# end
-#
-#
-#
-# #create range around coordinate points
-# samples_range = []
-# for i in sample_pts
-#    a, b = Tuple(i)
-#    c = [a:a+Stride-1,b:b+Stride-1]
-#    push!(samples_range, c)
-# end
-#
-#
-# #collect x and y values of each 9x9
-# exes = collect.(first.(samples_range))
-# wyes = collect.(last.(samples_range))
-# #create the 9x9
-# rep_exes = repeat.(exes, outer = 9)
-# rep_wyes = repeat.(wyes, inner = 9)
-# #zip them together
-# zip_reps = Tuple.(zip(rep_exes, rep_wyes))
-# samp_coords = [Tuple.(CartesianIndex.(zip_reps[i]...)) for i in 1:length(zip_reps)]
-# all_samp_pts = reduce(vcat, samp_coords)
-
 
 
 heatmap(Origin)
