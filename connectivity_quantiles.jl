2c43c793882e328b8d7893cbda33d5500a9e957e
kiristern <kiri.a.stern@gmail.com>
2020-07-28 23:23:12 -0400

    change directory & add comments

--- connectivity_quantiles.jl
+++ connectivity_quantiles.jl
@@ -5,26 +5,27 @@ using Statistics
 using StatsBase
 
 # Plot 1 - 9x9 example and scatterplot
-observed = readdlm("true_connect9x9_bear.csv")
-predicted = readdlm("model_connect9x9_bear.csv")
-heatmap(observed; lab = false, leg = false, clim = (0, 1), axes=false, aspectratio=1, frame=:none, c=:Greens, dpi=180)
+observed = readdlm("data/csv/true_connect9x9_bear.csv")
+predicted = readdlm("data/csv/model_connect9x9_bear.csv")
+p1 = heatmap(observed; lab = false, leg = false, clim = (0, 1), axes=false, aspectratio=1, frame=:none, c=:Greens, dpi=180)
 savefig("9_obs.png")
-heatmap(predicted; lab = false, leg = false, clim = (0, 1), axes=false, aspectratio=1, frame=:none, c=:Purples, dpi=180)
+p2 = heatmap(predicted; lab = false, leg = false, clim = (0, 1), axes=false, aspectratio=1, frame=:none, c=:Purples, dpi=180)
 savefig("9_prd.png")
 scatter(observed, predicted, c = :black, xlim = (0, 1), ylim = (0, 1), lab="", leg=false, frame=:grid, dpi=180, aspectratio=1)
-plot!([0, 1], [0, 1], c = :grey, ls = :dot)
+p3 = plot!([0, 1], [0, 1], c = :grey, ls = :dot)
 savefig("9_sct.png")
+plot(p1, p2, p3)
 
 # Plot 2 - prediction for the black bear
-observed = readdlm("connectivity_oursnoir.csv")[1:1242,:]
-predicted = readdlm("mod_connectivity_bear.csv")
-outside = findall(observed .== minimum(observed))
-observed[outside] .= NaN
+observed = readdlm("data/csv/connectivity_oursnoir.csv")[1:1242,:]
+predicted = readdlm("data/csv/mod_connectivity_bear.csv")
+outside = findall(observed .== minimum(observed)) #findall points outside of data (ie. zeros)
+observed[outside] .= NaN #set zeros to NaN
 predicted[outside] .= NaN
-mx = maximum(filter(!isnan, predicted))
-predicted = predicted ./ mx
-q = (m) -> ecdf(filter(!isnan, m))
-pr_q = q(predicted).(predicted)
+mx = maximum(filter(!isnan, predicted)) #get the max value from predicted
+predicted = predicted ./ mx #divide thru by max value to scale on between 0-1
+q = (m) -> ecdf(filter(!isnan, m)) #create function, q, to return empirical cumulative distribution fnc (ecdf) for values that are not NaN
+pr_q = q(predicted).(predicted) #apply fnc to transform output of NN into connectivity quantiles
 ob_q = q(observed).(observed)
 heatmap(pr_q, aspectratio=1, c=:BuPu, axis=nothing, frame=:none, leg=false, dpi=180)
 savefig("bear_predicted.png")
